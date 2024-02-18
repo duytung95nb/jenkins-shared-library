47dcc6d5f3abbed7dcab5d0e12c06502dbf28288
duytung95nb <duytung95nb@gmail.com>
2024-02-18 12:50:34 +0700

    Correct credentials for docker

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,7 @@
 final DOCKER_REGISTRY_NAMESPACE = "duytung95nb"
 
 def login() {
-    withCredentials([usernamePassword(credentialsId: '8a3bb0ae-9ff1-40e0-a0da-2ef7c1dc2cad', usernameVariable: 'username', passwordVariable: 'password')]) {
+    withCredentials([usernamePassword(credentialsId: '89b42c75-2ff6-4dea-9574-ec25ea4ff01f', usernameVariable: 'username', passwordVariable: 'password')]) {
         sh """
             docker login --username="${username}" --password="${password}"
         """
