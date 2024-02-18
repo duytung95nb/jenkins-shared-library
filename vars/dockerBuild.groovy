efb74f404181b8e5d3a46e6913b37e516e4eb6d2
duytung95nb <duytung95nb@gmail.com>
2024-02-18 13:12:24 +0700

    Specify docker

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,6 +2,8 @@ final DOCKER_REGISTRY_NAMESPACE = "duytung95nb"
 
 def login() {
     withCredentials([usernamePassword(credentialsId: '89b42c75-2ff6-4dea-9574-ec25ea4ff01f', usernameVariable: 'username', passwordVariable: 'password')]) {
+        def dockerHome = tool 'docker'
+        env.PATH = "${dockerHome}/bin:${env.PATH}"
         sh """
             docker login --username="${username}" --password="${password}"
         """
@@ -11,7 +13,8 @@ def login() {
 def build(String appName) {
     def scriptcontents = libraryResource "docker-compose.yaml"
     writeFile file:"docker-compose.yaml", text: scriptcontents
-
+    def dockerHome = tool 'docker'
+    env.PATH = "${dockerHome}/bin:${env.PATH}"
     sh """
         commit=${env.GIT_COMMIT} serviceName="${appName}" docker-compose build
     """
