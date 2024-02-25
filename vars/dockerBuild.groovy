b07fb400acf93f0a177651d971bcfbbc2193e9e1
duytung95nb <duytung95nb@gmail.com>
2024-02-25 11:08:47 +0700

    Try

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,23 +1,9 @@
 final DOCKER_REGISTRY_NAMESPACE = "duytung95nb"
 
-def login() {
-    withCredentials([usernamePassword(credentialsId: '89b42c75-2ff6-4dea-9574-ec25ea4ff01f', usernameVariable: 'username', passwordVariable: 'password')]) {
-        def dockerHome = tool 'docker'
-        env.PATH = "${dockerHome}/bin:${env.PATH}"
-        sh """
-            docker login --username="${username}" --password="${password}"
-        """
-    }
-}
-
 def build(String appName) {
-    def scriptcontents = libraryResource "docker-compose.yaml"
-    writeFile file:"docker-compose.yaml", text: scriptcontents
-    def dockerHome = tool 'docker'
-    env.PATH = "${dockerHome}/bin:${env.PATH}"
-    sh """
-        commit=${env.GIT_COMMIT} serviceName="${appName}" docker-compose build
-    """
+    docker.withRegistry("https://index.docker.io/v1", "89b42c75-2ff6-4dea-9574-ec25ea4ff01f") {
+        docker.build("${DOCKER_REGISTRY_NAMESPACE}/${appName}:${env.GIT_COMMIT}", 'app')
+    }
 }
 
 def push(String appName) {
