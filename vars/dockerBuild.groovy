4bbb315854397e7c0caa7e59d30554ce585c6b0a
duytung95nb <duytung95nb@gmail.com>
2024-03-06 00:18:53 +0700

    Run successfully pipeline with docker compose and docker

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,15 +1,16 @@
 final DOCKER_REGISTRY_NAMESPACE = "duytung95nb"
-
+final DOCKER_REGISTRY_URL = "https://index.docker.io/v1";
 def build(String appName) {
-    docker.withRegistry("https://index.docker.io/v1", "89b42c75-2ff6-4dea-9574-ec25ea4ff01f") {
-        sh "commit=${env.GIT_COMMIT} serviceName=${appName} /usr/local/bin/docker-compose build"
-        // docker.build("${DOCKER_REGISTRY_NAMESPACE}/${appName}:${env.GIT_COMMIT}", 'app')
+    docker.withRegistry(DOCKER_REGISTRY_URL, "89b42c75-2ff6-4dea-9574-ec25ea4ff01f") {
+        sh "commit=${env.GIT_COMMIT} serviceName=${appName} docker compose build"
     }
 }
 
 def push(String appName) {
-    sh """
-        docker push "${DOCKER_REGISTRY_NAMESPACE}/${appName}-nginx:${env.GIT_COMMIT}"
-        docker push "${DOCKER_REGISTRY_NAMESPACE}/${appName}:${env.GIT_COMMIT}"
-    """
+    docker.withRegistry(DOCKER_REGISTRY_URL, "89b42c75-2ff6-4dea-9574-ec25ea4ff01f") {
+        sh """
+            docker push "${DOCKER_REGISTRY_NAMESPACE}/${appName}-nginx:${env.GIT_COMMIT}"
+            docker push "${DOCKER_REGISTRY_NAMESPACE}/${appName}:${env.GIT_COMMIT}"
+        """
+    }
 }
