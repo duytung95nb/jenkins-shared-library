58ccd5c06f2198ad85a76af04a09cabebe5f74d2
duytung95nb <duytung95nb@gmail.com>
2024-03-07 00:13:19 +0700

    Fix registry url for pushing

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,11 +1,9 @@
 def build(String appName) {
-    docker.withRegistry("https://index.docker.io/v1", "89b42c75-2ff6-4dea-9574-ec25ea4ff01f") {
-        sh "commit=${env.GIT_COMMIT} serviceName=${appName} docker compose build"
-    }
+    sh "commit=${env.GIT_COMMIT} serviceName=${appName} docker compose build"
 }
 
 def push(String appName) {
-    docker.withRegistry("https://index.docker.io/v1", "89b42c75-2ff6-4dea-9574-ec25ea4ff01f") {
+    docker.withRegistry("https://index.docker.io/v1/", "89b42c75-2ff6-4dea-9574-ec25ea4ff01f") {
         sh """
             commit=${env.GIT_COMMIT} serviceName=${appName} docker compose push
         """
